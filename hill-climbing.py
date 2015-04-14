efb1b438179543a12434f2a5892f628f081f0e53
Simeon <simeon.tsvetanov@gmail.com>
2015-04-15 01:15:14 +0300

    My first commit

--- hill-climbing.py
+++ hill-climbing.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-Hill climbing or Gradient Desent is local search method.
+Hill climbing aka Gradient Desent is local search method.
 
 @author: Simeon Tsvetanov
 """
+iter = 5
+
 import random
 solutions = [3,5,6,7,6,4,3,5,6,7,9,23,35,12,6,5,4,23,24,15,13,8,9,12,15,4]
 
 """
-Generate random solutions form the list
-s  is the index of the element
-solutions[s] is the value of the solution s
+Repeat the local search to extend the algorithm to Iterated Local Search
 """
-s = solutions[random.randrange(len(solutions) -1)]
-print("Initial Solution Value: ", solutions[s], "Solution Index: ", s)
-while(solutions[s] < solutions[s+1]):
-    s=s+1
-    print("Current best solution value: ", solutions[s])
+for i in range(0, iter):
+    HillClimbing(solutions)
     
-print("Local maximum found: ", solutions[s])
+def HillClimbing(solutions):
+    """
+    Generate random solutions form the list
+    s  is the index of the element
+    solutions[s] is the value of the solution s
+    """
+    s = solutions[random.randrange(len(solutions) -1)]
+    print("Initial Solution Value: ", solutions[s], "Solution Index: ", s)
+    while(solutions[s] < solutions[s+1]):
+        s=s+1
+        print("Current best solution value: ", solutions[s])
+        
+    print("Local maximum found: ", solutions[s])
 
 """
 TO DO
