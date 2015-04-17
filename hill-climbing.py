2afe7fc410dde946453cc627086e58b4c1fb8f4e
hpctusofia <hpctusofia@gmail.com>
2015-04-17 17:28:37 +0300

    Added both directions search and ITL

--- hill-climbing.py
+++ hill-climbing.py
@@ -4,30 +4,34 @@ Hill climbing aka Gradient Desent is local search method.
 
 @author: Simeon Tsvetanov
 """
-iter = 5
 
 import random
-solutions = [3,5,6,7,6,4,3,5,6,7,9,23,35,12,6,5,4,23,24,15,13,8,9,12,15,4]
+solutions = [3,5,6,7,8,6,4,3,2,5,6,7,9,23,35,12,6,5,4,10,15,13,8,9,12,15,4]
 
 """
 Repeat the local search to extend the algorithm to Iterated Local Search
 """
-for i in range(0, iter):
-    HillClimbing(solutions)
     
-def HillClimbing(solutions):
-    """
-    Generate random solutions form the list
-    s  is the index of the element
-    solutions[s] is the value of the solution s
-    """
-    s = solutions[random.randrange(len(solutions) -1)]
-    print("Initial Solution Value: ", solutions[s], "Solution Index: ", s)
-    while(solutions[s] < solutions[s+1]):
-        s=s+1
+def HillClimbing():
+    s = random.randrange(len(solutions) - 1)    
+    print("Initial Value", solutions[s], "Initial Index", s)
+    while(solutions[s] < solutions[s + 1] or solutions[s] < solutions[s - 1]):
+        if (solutions[s] < solutions[s + 1]):       
+            s = s + 1
+        else:
+            s = s - 1
         print("Current best solution value: ", solutions[s])
-        
-    print("Local maximum found: ", solutions[s])
+	return solutions[s]
+ 
+iter = 5
+Best_ITL = 0
+
+for x in range(0, iter):
+    s = HillClimbing()
+    if(Best_ITL < s):
+        Best_ITL = s
+       
+print("Best solutions", Best_ITL)
 
 """
 TO DO
